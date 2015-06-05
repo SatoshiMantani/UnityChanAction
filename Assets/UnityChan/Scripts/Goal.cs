8296a19bcfad10433219216160dc35f018dac130
SatoshiMantani <satoshimantani@gmail.com>
2015-06-05 18:27:28 +0900

    ゴール時のアニメーションを追加

---
+++ Assets/UnityChan/Scripts/Goal.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCTriggerEnter(Collider col){
+
+		col.SendMessage ("OnGoal");
+		//Debug.Log ("Goal!");
+
+	}
+}
